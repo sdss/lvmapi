00cae26d10f695a43a52e24ff52248cd4d06e9cb
José Sánchez-Gallego <gallegoj@uw.edu>
2024-07-24 19:58:05 +0000

    Add block option to /macros/shutdown

--- src/lvmapi/routers/macros.py
+++ src/lvmapi/routers/macros.py
@@ -8,6 +8,8 @@
 
 from __future__ import annotations
 
+import asyncio
+
 from fastapi import APIRouter, HTTPException, Request
 
 from lvmapi.tools.gort import get_gort_client
@@ -17,12 +19,14 @@ router = APIRouter(prefix="/macros", tags=["macros"])
 
 
 @router.get("/shutdown")
-async def shutdown(request: Request) -> bool:
+async def shutdown(request: Request, block: bool = True) -> bool:
     """Performs an emergency shutdown of the enclosure and telescopes."""
 
     try:
         async with get_gort_client(request.app) as gort:
-            await gort.shutdown(park_telescopes=True)
+            task = asyncio.create_task(gort.shutdown(park_telescopes=True))
+            if block:
+                await task
     except Exception as ee:
         raise HTTPException(status_code=500, detail=str(ee))
 
