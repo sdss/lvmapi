cb4691abf8fc217dfbbbe6621cbed0b68858008e
José Sánchez-Gallego <gallegoj@uw.edu>
2024-10-21 19:46:50 +0000

    Allow to pass start and end time to the weather report endpoint

--- src/lvmapi/routers/weather.py
+++ src/lvmapi/routers/weather.py
@@ -8,8 +8,10 @@
 
 from __future__ import annotations
 
+from typing import Annotated
+
 import polars
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
 
 from lvmapi.tools.weather import get_weather_data
 
@@ -17,16 +19,45 @@ from lvmapi.tools.weather import get_weather_data
 router = APIRouter(prefix="/weather", tags=["weather"])
 
 
-@router.get("/")
-@router.get("/report")
-async def get_weather(
-    station: str = "DuPont",
-    delta_time: float = 3600,
-    last: bool = False,
+@router.get("/", summary="Weather report")
+@router.get("/report", summary="Weather report")
+async def route_get_weather(
+    station: Annotated[
+        str,
+        Query(description="The weather station to query"),
+    ] = "DuPont",
+    start_time: Annotated[
+        str | None,
+        Query(description="Start time for the query. ISO format."),
+    ] = None,
+    end_time: Annotated[
+        str | None,
+        Query(description="End time for the query. ISO format."),
+    ] = None,
+    delta_time: Annotated[
+        float,
+        Query(
+            description="Look-back number of seconds. "
+            "Ignored if start_time/end_time are defined."
+        ),
+    ] = 3600,
+    last: Annotated[
+        bool,
+        Query(description="Return only the last entry."),
+    ] = False,
 ) -> list[dict]:
     """Returns the weather report from a weather station."""
 
-    df = await get_weather_data(station=station, start_time=delta_time)
+    if start_time and end_time:
+        df = await get_weather_data(
+            station=station,
+            start_time=start_time,
+            end_time=end_time,
+        )
+    elif any([start_time, end_time]) and not all([start_time, end_time]):
+        raise HTTPException(400, "start_time and end_time must be both defined.")
+    else:
+        df = await get_weather_data(station=station, start_time=delta_time)
 
     if last and len(df) > 0:
         df = df.tail(1)
